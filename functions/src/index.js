e3d55e9e93716e014666a2c0ae8c4fee770ce395
Nicola Corti <corti.nico@gmail.com>
2019-04-11 21:11:01 +0200

    Add Trigger Notifications function

--- functions/src/index.js
+++ functions/src/index.js
@@ -4,6 +4,7 @@ import admin from 'firebase-admin';
 import saveUserData from './save-user-data';
 import sendGeneralNotification from './notifications';
 import scheduleNotifications from './schedule-notifications';
+import triggerNotifications from './trigger-notifications';
 import optimizeImages from './optimize-images';
 import mailchimpSubscribe from './mailchimp-subscribe';
 import prerender from './prerender';
@@ -14,6 +15,7 @@ export {
   saveUserData,
   sendGeneralNotification,
   scheduleNotifications,
+  triggerNotifications,
   optimizeImages,
   mailchimpSubscribe,
   prerender,
